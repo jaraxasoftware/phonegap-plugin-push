2cbc671b1c4017c46ac356b426ba220a28311bf4
jrxjsj <juan.sanchez@jaraxa.com>
2017-02-01 19:39:19 +0100

    Added preliminary SENDER_ID variable handling for browser platform

--- hooks/browser/createManifest.js
+++ hooks/browser/createManifest.js
@@ -2,9 +2,26 @@ module.exports = function(context) {
     console.log('Updating SENDER_ID for browser platform...');
 	var fs = require('fs');
     var path = require('path');
-	console.log(JSON.stringify(context.opts));
+	var senderIdValue = '';
+	var children = context.opts.plugin.pluginInfo._et._root._children;
+	for (var i = 0; i < children.length; i++) {
+		if (children[i].tag == "preference" && children[i].attrib.name == "SENDER_ID") {
+			senderIdValue = children[i].attrib.default;
+			break;
+		}
+	}
+	console.log('Default SENDER_ID: ' + senderIdValue);
+	process.argv.forEach(function (val, index, array) {
+		if (val == "--variable") {
+			if (array.length > index+1 && array[index+1].indexOf("SENDER_ID") == 0) {
+				senderIdValue = array[index+1].substring(array[index+1].indexOf("=") + 1);
+			}
+		}
+	});	
+	console.log('Detected SENDER_ID: ' + senderIdValue);
 	
 	var manifestContent = fs.readFileSync(path.join(context.opts.plugin.dir, 'src/browser/manifest.json'), 'utf-8');
-	manifestContent = manifestContent.replace("$SENDER_ID", "test");
-	console.log(manifestContent);	
+	manifestContent = manifestContent.replace("$SENDER_ID", senderIdValue);
+	fs.writeFileSync(path.join(context.opts.projectRoot, 'platforms/browser/www/manifest.json'), manifestContent, 'utf-8');
+	console.log("Updated SENDER_ID for browser platform");
 }
